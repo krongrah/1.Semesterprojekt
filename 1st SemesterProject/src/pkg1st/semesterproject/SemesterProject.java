8ce720083f6913739687773863e961ad222df601
Krongrah <Krongrah@10.125.18.172>
2017-10-05 12:01:39 +0200

    test2

--- 1st SemesterProject/src/pkg1st/semesterproject/SemesterProject.java
+++ 1st SemesterProject/src/pkg1st/semesterproject/SemesterProject.java
@@ -17,6 +17,7 @@ public class SemesterProject {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("test");
+        System.out.println("test2");
     }
     
 }
