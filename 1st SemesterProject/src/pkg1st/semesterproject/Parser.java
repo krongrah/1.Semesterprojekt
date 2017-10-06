ee073887d9fccb9af2252b964bdced73e0114e3c
Krongrah <Krongrah@10.125.18.172>
2017-10-06 11:53:49 +0200

    Commenting Rooms

--- 1st SemesterProject/src/pkg1st/semesterproject/Parser.java
+++ 1st SemesterProject/src/pkg1st/semesterproject/Parser.java
@@ -11,23 +11,23 @@ public class Parser
 {
     private CommandWords commands;
     private Scanner reader;
-
+    //contructor, generates a CommandWords and a scanner
     public Parser() 
     {
         commands = new CommandWords();
         reader = new Scanner(System.in);
     }
-
+    //commented throughout
     public Command getCommand() 
-    {
+    {   
         String inputLine;
         String word1 = null;
         String word2 = null;
 
         System.out.print("> ");
-
+        //gets string from the player
         inputLine = reader.nextLine();
-
+        //splits the string into one or two words
         Scanner tokenizer = new Scanner(inputLine);
         if(tokenizer.hasNext()) {
             word1 = tokenizer.next();
@@ -35,10 +35,10 @@ public class Parser
                 word2 = tokenizer.next();
             }
         }
-
+        //returns a the command derived from the string, through the command class
         return new Command(commands.getCommandWord(word1), word2);
     }
-
+    //prints all Commands
     public void showCommands()
     {
         commands.showAll();
