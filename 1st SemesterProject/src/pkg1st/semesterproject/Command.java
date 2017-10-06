514fc3ca072bd35f2a23a04c041008b3c3a47b54
Krongrah <Krongrah@10.125.18.172>
2017-10-06 11:10:20 +0200

    Commenting Command

--- 1st SemesterProject/src/pkg1st/semesterproject/Command.java
+++ 1st SemesterProject/src/pkg1st/semesterproject/Command.java
@@ -6,31 +6,31 @@ package pkg1st.semesterproject;
  */
 
 public class Command
-{
+{   //has two attributes, a commandWord and a matching string
     private CommandWord commandWord;
     private String secondWord;
-
+    //Constructor
     public Command(CommandWord commandWord, String secondWord)
     {
         this.commandWord = commandWord;
         this.secondWord = secondWord;
     }
-
+    //getter
     public CommandWord getCommandWord()
     {
         return commandWord;
     }
-
+    //setter
     public String getSecondWord()
     {
         return secondWord;
     }
-
+    //returns true the command is the unknown command.
     public boolean isUnknown()
     {
         return (commandWord == CommandWord.UNKNOWN);
     }
-
+    //returns true if the command has a string attached. 
     public boolean hasSecondWord()
     {
         return (secondWord != null);
