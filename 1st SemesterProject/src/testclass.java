6d18cdecd9317fff81fb4d85703bcee0f63b7295
jensd <jensd@XPS-15>
2017-10-06 11:17:21 +0200

    Jens test

---
+++ 1st SemesterProject/src/testclass.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jensd
+ */
+public class testclass {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
