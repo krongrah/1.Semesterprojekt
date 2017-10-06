4bd39ebcc65af73c4b8318a60a02a6593a1bd65c
Krongrah <Krongrah@10.125.18.172>
2017-10-06 11:22:25 +0200

    Commenting Rooms

--- 1st SemesterProject/src/pkg1st/semesterproject/Room.java
+++ 1st SemesterProject/src/pkg1st/semesterproject/Room.java
@@ -10,31 +10,31 @@ import java.util.Iterator;
  * @version 2006.03.30
  */
 public class Room 
-{
+{   //rooms have a description/name and map with strings to rooms, serving as exits.
     private String description;
     private HashMap<String, Room> exits;
-
+    //contructor, requires description and generates an Exits map.
     public Room(String description) 
     {
         this.description = description;
         exits = new HashMap<String, Room>();
     }
-
+    //adds a neighbooring room to the Exits map, with the direction being the key
     public void setExit(String direction, Room neighbor) 
     {
         exits.put(direction, neighbor);
     }
-
+    //getter for the description/name
     public String getShortDescription()
     {
         return description;
     }
-
+    //Getter for the description/name along with a list of exits.
     public String getLongDescription()
     {
         return "You are " + description + ".\n" + getExitString();
     }
-
+        //returns a list of exits
 	private String getExitString()
     {
         String returnString = "Exits:";
@@ -44,7 +44,7 @@ public class Room
         }
         return returnString;
     }
-
+    //returns the room in the given direction.
     public Room getExit(String direction) 
     {
         return exits.get(direction);
