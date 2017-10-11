ea2538ddad767d2ba4c88a09af4b8c9ff2299f40
jensd <jensd@XPS-15>
2017-10-11 22:51:19 +0200

    NPC klassen lavet. move metoden mangler indhold, kode ikke kommenteret

--- 1st SemesterProject/src/pkg1st/semesterproject/NPC.java
+++ 1st SemesterProject/src/pkg1st/semesterproject/NPC.java
@@ -4,11 +4,38 @@
  * and open the template in the editor.
  */
 package pkg1st.semesterproject;
+import java.util.*;
 
 /**
  *
  * @author Krongrah
  */
 public class NPC {
+    static String name;
+    static List<String> dialougeList = new ArrayList<String>();
+    static int suspectability;
+    static int dialougeStep;
     
+    public static void move(String Room) {
+        
+    }
+    public static String getName(){
+        return name;
+    }
+    public static int getSuspectability() {
+        return suspectability;
+    } 
+    public static int getDialougeStep() {
+        return dialougeStep;
+    }
+    public static List<String> getDialougeList() {
+        return dialougeList;
+    }
+    public static void incDialougeStep() {
+        dialougeStep += 1;
+    }
+    public NPC(String newName, List<String> newDialougeList) {
+        this.name = newName;
+        this.dialougeList = newDialougeList;
+    }
 }
