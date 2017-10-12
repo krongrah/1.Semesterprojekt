a27e73ff55a2f8563665f73fe9f76399e4f1af8e
Krongrah <Krongrah@10.125.18.172>
2017-10-12 12:17:27 +0200

    Clue Class

--- 1st SemesterProject/src/pkg1st/semesterproject/Clue.java
+++ 1st SemesterProject/src/pkg1st/semesterproject/Clue.java
@@ -11,4 +11,26 @@ package pkg1st.semesterproject;
  */
 public class Clue {
     
+    //Attributes
+    private String name;
+    private String description;
+    
+    //constructor
+    Clue(String _name, String _description){
+    name=_name;
+    description=_description;
+    }
+    
+    //getters
+    public String getName(){
+    return name;
+    }
+    
+    public String getDescription(){
+    return description;
+    }
+    
+    
+    
+    
 }
