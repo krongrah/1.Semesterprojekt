49c438550deaaf722409df6d492bb52c8aaf473c
Krongrah <Krongrah@10.125.18.172>
2017-10-12 12:58:50 +0200

    Tiny fix in NPC

--- 1st SemesterProject/src/pkg1st/semesterproject/NPC.java
+++ 1st SemesterProject/src/pkg1st/semesterproject/NPC.java
@@ -12,31 +12,31 @@ import java.util.*;
  */
 public class NPC {
     // NPC attributes
-    static String name;
-    static List<String> dialogueList = new ArrayList<String>();
-    static int suspectability;
-    static int dialogueStep;
+    private String name;
+    String[] dialogueList;
+    private int suspectability;
+    private int dialogueStep;
     //Moves NPC to another room
     public static void move(String Room) {
         
     }
-    public static String getName(){
+    public String getName(){
         return name;
     }
-    public static int getSuspectability() {
+    public int getSuspectability() {
         return suspectability;
     } 
-    public static int getDialogueStep() {
+    public int getDialogueStep() {
         return dialogueStep;
     }
-    public static List<String> getDialogueList() {
-        return dialogueList;
+    public String getDialogue() {
+        return dialogueList[dialogueStep];
     }
-    public static void incrementDialogueStep() {
+    public void incrementDialogueStep() {
         dialogueStep += 1;
     }
     // NPC constructor
-    public NPC(String newName, List<String> newDialogueList) {
+    public NPC(String newName, String[] newDialogueList) {
         this.name = newName;
         this.dialogueList = newDialogueList;
     }
