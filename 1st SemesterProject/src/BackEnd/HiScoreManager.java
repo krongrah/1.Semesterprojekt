6f03f7ce45886daf72a8dd9e4e1fc79dc8403074
Krongrah <Krongrah@gmail.com>
2017-11-15 15:08:30 +0100

    outLined HiScoreManager and made another interface

---
+++ 1st SemesterProject/src/BackEnd/HiScoreManager.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BackEnd;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Krongrah
+ */
+public class HiScoreManager {
+    List<String> HiScore=new ArrayList<>();
+    void store(){
+    
+    }
+    void sort(){}
+    void pull(){}
+    void add(){}
+}
