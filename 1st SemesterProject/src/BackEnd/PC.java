e6a8b6727253e90fc8421d71d09ceb6663b2e960
Krongrah <Krongrah@gmail.com>
2017-12-06 15:34:16 +0100

    finished inspect

--- 1st SemesterProject/src/BackEnd/PC.java
+++ 1st SemesterProject/src/BackEnd/PC.java
@@ -58,10 +58,10 @@ public class PC {
     //getters for the descriptions
 
     public void inspectItemMap(String entry){
-        System.out.println(journal.get(entry).getDescription());
+        System.out.println(inventory.get(entry).getDescription());
     }
     public void inspectEntryMap(String item){
-        System.out.println(inventory.get(item).getDescription());
+        System.out.println(journal.get(item).getDescription());
     }
 
     public void moveToRoom(Item thing, Room currentRoom) {
